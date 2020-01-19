9aa9e245ee0a581315d7ae52657dc12d68a52383
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-01-19 21:31:38 +0530

    Delete new.php

--- new.php
+++
@@ -1,9 +0,0 @@
-
-<script src ="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script>
- $.getJSON("https://cricapi.com/api/fantasySummary/?apikey=6aP8B4MImxSNxI6fevBmC2ZshO42&unique_id="+<?php echo $_GET['key'];?>, function(data1) {
-   
-  var  xx = <?php echo $_GET['ind'];?>;
-   document.writeln(xx);
-    });
-</script>
