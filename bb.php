88b197a976115dbfe69549d28f9de96ab97bdcdd
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-02-05 14:02:24 +0530

    Add files via upload

---
+++ bb.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.ms
+		{
+			border:2px solid;
+			color: white;
+			background-color: #008B8B;
+			padding:4px 4px;
+		}
+		.st
+		{
+			color:green;
+			font-weight:bold;
+		}
+		.summary
+          {
+          	color: red;
+          	font-weight: bold;
+          }
+          .but
+          {
+          margin-right: 5px;
+          color: #008B8B;
+          padding:2px 2px;
+          background-color:white;
+          font-weight:bold;
+         
+          }
+	</style>
+	<script src = "https://code.jquery.com/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<script>
+	function getdetails(jk,sid,mid){
+            document.location.href = "scoreboard.php?sid="+sid+"&mid="+mid+"&j="+jk;
+          
+	}
+function getscore(sid,mid){
+	  return new Promise( resolve => {
+    
+                      fetch("https://dev132-cricket-live-scores-v1.p.rapidapi.com/scorecards.php?seriesid="+sid+"&matchid="+mid, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "dev132-cricket-live-scores-v1.p.rapidapi.com",
+		"x-rapidapi-key": "58c637e2a4mshdc73000604d617bp166404jsn6d692798bbc2"
+	}
+})
+.then(response => {
+	return(response.json());
+
+})
+.then(function(data2){
+	resolve(data2);
+	 
+	});
+});
+
+	}
+
+fetch("https://dev132-cricket-live-scores-v1.p.rapidapi.com/matches.php?completedlimit=6&inprogresslimit=7&upcomingLimit=9", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "dev132-cricket-live-scores-v1.p.rapidapi.com",
+		"x-rapidapi-key": "58c637e2a4mshdc73000604d617bp166404jsn6d692798bbc2"
+	}
+})
+.then((response) =>{
+ return response.json();
+})
+.then(async  MyJson =>{
+     console.log(MyJson);
+     for(let i = 0;i<MyJson.matchList.matches.length;i++){
+                var div = document.createElement('div');
+                 let   sid = MyJson.matchList.matches[i].series.id;
+                  let mid = MyJson.matchList.matches[i].id;
+                div.className = 'c'+i;
+                var match = document.createElement('p');
+                match.className = 'ms';
+                match.innerHTML = MyJson.matchList.matches[i].series.name+"          "+MyJson.matchList.matches[i].venue.name;
+                 document.body.appendChild(div);
+                 $('.c'+i).append(match)
+                 var team1 = MyJson.matchList.matches[i].homeTeam.name;
+                   var team2 = MyJson.matchList.matches[i].awayTeam.name;
+                 if(MyJson.matchList.matches[i].status== 'LIVE'){
+                   var status = document.createElement('p');
+                   status.innerHTML = 'LIVE';
+                   status.className = 'st';
+                   $('.c'+i).append(status);
+                         }
+                         if(MyJson.matchList.matches[i].status!= 'UPCOMING'){
+                   if(MyJson.matchList.matches[i].scores.homeOvers!= 0.0){
+                                var score = document.createElement('p');
+                                score.innerHTML =team1+"  "+ MyJson.matchList.matches[i].scores.homeScore+"("+ MyJson.matchList.matches[i].scores.homeOvers+")";
+                                $('.c'+i).append(score);
+                                  }
+                   if(MyJson.matchList.matches[i].scores.awayOvers!= 0.0)
+                   {var score = document.createElement('p');
+                                score.innerHTML =team2+"  "+ MyJson.matchList.matches[i].scores.awayScore+"("+ MyJson.matchList.matches[i].scores.awayOvers+")";
+                                 $('.c'+i).append(score);
+                   }
+                   var sum = document.createElement('p');
+                   sum.innerHTML = MyJson.matchList.matches[i].matchSummaryText;
+                   sum.className = 'summary';
+                   $('.c'+i).append(sum);
+               
+
+              
+                 getscore(sid,mid).then( data2 =>{
+
+                console.log(data2);
+                for(let  j = 0;j<data2.fullScorecard.innings.length;j++){
+                  	 var button = document.createElement('button');
+                  	 button.innerHTML =data2.fullScorecard.innings[j].name;
+                  	 button.className  = 'ing'+i+j;
+                  	 
+                  	  $('.c'+i).append(button);
+                  	 $('.ing'+i+j).click(function(){
+                  	 	getdetails(j,MyJson.matchList.matches[i].series.id, MyJson.matchList.matches[i].id);                 	
+                  	 	 });
+                  	 button.className = 'but'; 
+
+                  }
+                  
+                 	});
+               	     
+                 }
+              
+                if(MyJson.matchList.matches[i].status== 'UPCOMING'){
+                 	var status = document.createElement('p');
+                   status.innerHTML = 'UPCOMING';
+                   status.style.color = 'red';
+                   $('.c'+i).append(status);
+                 }
+}//for loop
+     
+});
+
+</script>
+</body>
+</html>
---
+++ scoreboard.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name= 'viewport' content = "width=device-width,initial-scale=1.0">
+	<style>
+		.b
+		{
+			border:1px solid white;
+			background-color:grey;
+			color:white;
+			font-weight: bold;
+			padding:2px 3px;
+			height: 30px;
+
+
+		}
+		
+		.b1
+		{
+			position: relative;
+			bottom:5px;
+		}
+	  .tab
+	  {text-align:center;
+
+
+	  }	
+	  @media(max-width: 500px){
+	  	  .scr{
+	  
+	  	display: inline;
+	  	position: relative;
+	  	left:70px;
+	  	font-size:8px;
+	  }
+	
+	@media(max-width: 500px){
+		.row,.rows,.tab,.vin
+	  {
+	  	padding-right: 1vw;
+	  	padding-bottom: 0.5vw;
+	  	border:1px;
+	  	height:0.3vw;
+	  	 border-collapse:separate;
+	  	 border-spacing: 0 1vw;
+	  	 text-align:left;
+         font-size:1vw;
+	  	 
+	  }
+
+	}
+	 
+	 .tab>:nth-child(odd){
+ background-color: lightgrey;
+ width:200px;
+
+
+}
+
+@media(max-width:500px){
+.head{
+	padding:4px;
+	margin-bottom:4px;
+	color:white;
+	font-weight:bold;
+	font-size:2vw;
+	margin-top: 0;
+	border:1px solid white;
+	background-color: #008B8B;
+	width:80vw;
+	height:2vw;
+	
+}
+}
+	</style>
+	
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+<script src = "https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script>
+	  var inng = <?php echo $_GET['j'];?>;
+              fetch("https://dev132-cricket-live-scores-v1.p.rapidapi.com/scorecards.php?seriesid="+<?php echo $_GET['sid'];?>+"&matchid="+<?php echo $_GET['mid'];?>, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "dev132-cricket-live-scores-v1.p.rapidapi.com",
+		"x-rapidapi-key": "58c637e2a4mshdc73000604d617bp166404jsn6d692798bbc2"
+	}
+}).then(response =>{
+	return (response.json());
+	
+	
+}).then(Myjson =>{
+    var head = document.createElement('div');
+	var x = document.createElement('p');
+	x.innerHTML =Myjson.fullScorecard.innings[inng].run+" - "+Myjson.fullScorecard.innings[inng].wicket+'('+Myjson.fullScorecard.innings[inng].over+')';
+	head.innerHTML = 'Batting';
+	head.className = 'head';
+	x.className = 'scr';
+	document.body.appendChild(head);
+	$('.head').append(x);	
+ 
+    var table = document.createElement('table');
+    table.className = 'tab';
+   var tr = document.createElement('tr');
+   var t1 = document.createElement('th');
+   var tt1 = document.createElement('th');
+   var t2 = document.createElement('th');
+   var t3 = document.createElement('th');
+   var t4 = document.createElement('th');
+   var t5 = document.createElement('th');
+   var t6 = document.createElement('th');
+   var txt1 = document.createTextNode('Batsmen');
+    var txt7 = document.createTextNode('');
+   var txt2 = document.createTextNode('R');
+   var txt3 = document.createTextNode('B');
+   var txt4 = document.createTextNode('4s');
+   var txt5 = document.createTextNode('6s');
+   var txt6 = document.createTextNode('SR');
+  
+   t1.appendChild(txt1);
+   tt1.appendChild(txt7);
+   t2.appendChild(txt2);
+   t3.appendChild(txt3);
+   t4.appendChild(txt4);
+   t5.appendChild(txt5);
+   t6.appendChild(txt6);
+
+    t1.className = 'rows';
+       t2.className = 'rows';
+        t3.className = 'rows';
+          t4.className = 'rows';
+       t5.className = 'rows';
+        t6.className = 'rows';
+        tt1.className = 'rows';
+
+   tr.appendChild(t1);
+   tr.appendChild(tt1)
+      tr.appendChild(t2);
+      tr.appendChild(t3);
+      tr.appendChild(t4);
+      tr.appendChild(t5);
+      tr.appendChild(t6);
+       table.appendChild(tr);
+        
+
+  for(var  j1 = 0;j1<Myjson.fullScorecard.innings[inng].batsmen.length;j1++){
+  	
+  	if(Myjson.fullScorecard.innings[inng].batsmen[j1].strikeRate!="-" && Myjson.fullScorecard.innings[inng].batsmen[j1].balls!=0){
+     
+  
+      var tr = document.createElement('tr');
+      tr.className = 'trow';
+      var td1 = document.createElement('td');
+      var td = document.createElement('td');
+      var td2 = document.createElement('td');
+      var td3 = document.createElement('td');
+      var td4 = document.createElement('td');
+      var td5 = document.createElement('td');
+      var td6= document.createElement('td');
+
+      var text1 = document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].name);
+      var text2 = document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].runs);
+      var text3 = document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].balls);
+      var text4 =  document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].howOut);
+       var text5 = document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].fours);
+      var text6= document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].sixes);
+      var text7 =  document.createTextNode(Myjson.fullScorecard.innings[inng].batsmen[j1].strikeRate);
+
+      td1.className = 'row';
+       td2.className = 'row';
+        td3.className = 'row';
+        td4.className = 'row';
+       td5.className = 'row';
+        td6.className = 'row';
+        td.className = 'row';
+      td1.appendChild(text1);
+      td.appendChild(text4);
+      td2.appendChild(text2);
+      td3.appendChild(text3);
+      td4.appendChild(text5);
+      td5.appendChild(text6);
+      td6.appendChild(text7);
+
+      tr.appendChild(td1);
+      tr.appendChild(td);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      tr.appendChild(td5);
+      tr.appendChild(td6);
+      table.appendChild(tr);
+  document.body.appendChild(table);
+      }
+  	
+  }
+  var head1 = document.createElement('p');
+  head1.innerHTML = 'Bowling';
+  head1.className =   'head';
+  document.body.appendChild(head1);
+  var arr = ['.name','.runsConceded','.maidens','.wickets','overs','noBalls','wides','economy'];
+  var arr1 = ['Bowler','R','M','W','O','NB','Wd','ECO'];
+  var table2 = document.createElement('table');
+  table2.className = 'tab';
+  var tr = document.createElement('tr');
+  for(var it = 0;it<=7;it++) {
+   var hd = document.createElement('th');
+   var bt = document.createTextNode(arr1[it]);
+   hd.appendChild(bt);
+   tr.appendChild(hd);
+   table2.appendChild(tr);
+   hd.className = 'vin';
+}
+   for(var j2=0;j2<Myjson.fullScorecard.innings[inng].bowlers.length;j2++){
+   	var tr = document.createElement('tr');
+   	for(var v=0;v<=7;v++){
+
+   		var td = document.createElement('td');
+   		td.className = 'vin';
+   		if(v==0){
+   		var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].name);
+   		   }
+   		else if(v==1){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].runsConceded);}
+   		else if(v==2){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].maidens);}
+   		else if(v==3){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].wickets);}
+   		else if(v==4){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].overs);}
+   		else if(v==5){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].noBalls);}
+   		else if(v==6){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].wides);}
+   		else if(v==7){var tabnode  = document.createTextNode(Myjson.fullScorecard.innings[inng].bowlers[j2].economy);}
+   		td.appendChild(tabnode);
+   		tr.appendChild(td);
+   		table2.appendChild(tr);
+   	}
+
+   }
+
+   document.body.appendChild(table2);
+
+  
+ 
+});
+
+ </script>
+
+
